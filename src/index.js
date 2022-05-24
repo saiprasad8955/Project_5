9267f5c6d520eea340c74d8447224cff965f1074
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-05-24 12:14:24 +0530

    aws-s3

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const express =  require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const route = require("./route/route")
 const app = express();
 
 const mongoose = require("mongoose")
 const multer = require("multer")
-
+const { AppConfig } = require('aws-sdk');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect("mongodb+srv://Sai0047:rXxgqYKPqwnhcXX7@cluster0.qptsw.mongodb.net/group8Database",
-{ useNewUrlParser : true })
+    { useNewUrlParser: true })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use('/',route);
+app.use('/', route);
 app.use(multer().any());
 
-app.listen ( process.env.PORT || 3000, function (){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express App Running On Port" + (process.env.PORT || 3000))
 })
