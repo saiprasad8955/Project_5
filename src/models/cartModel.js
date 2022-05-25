32be6433c162425a0e993f200f08427470744a12
“patilpriyas” <patilpriya141994@gmail.com>
2022-05-25 23:17:37 +0530

    cart

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,17 +1,33 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 const cartSchema = new mongoose.Schema({
-
-
-
-
-
-
-
-
-
-
-
+    userId: {
+        type:ObjectId,
+        ref:"User",
+        required:true,
+        unique:true
+    },
+    items:[{
+        productId:{ 
+            type:ObjectId,
+            ref:"Product",
+            required:true,
+        },
+        quantity:{
+            type: Number,
+            required:true,
+            min:1
+        } 
+    }],
+    totalPrice:{
+        type: Number,
+        required:true,
+    },                  //Holds total price of all the items in the cart
+    totalItems:{
+        type: Number,
+        required:true,
+    },                  //Holds total number of items in the cart
 
 }, { timestamps: true });
 
