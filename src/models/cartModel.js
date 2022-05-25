842259e5bddf9b49c5b97e37e7b7bdeb57b4c447
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-05-26 00:37:04 +0530

    All models done

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -2,17 +2,34 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
 
-
-
-
-
-
-
-
-
-
-
-
+    userId: {
+        type: ObjectId,
+        refs: 'USER',
+        required: true,
+        unique: true
+    },
+    items: [{
+        productId: {
+            type: ObjectId,
+            refs: 'PRODUCT',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            minlength: 1
+        }
+    }],
+    totalPrice: {
+        type: Number,
+        required: true,
+        // comment: "Holds total price of all the items in the cart"
+    },
+    totalItems: {
+        type: Number,
+        required: true,
+        // comment: "Holds total number of items in the cart"
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model("CART", cartSchema)
