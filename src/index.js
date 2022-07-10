42dfe0f9747b16ebad941da682db6e0768cdb5ce
saiprasad <rsaiprasad4@gmail.com>
2022-07-10 21:40:17 +0530

    ENV

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => console.log("MongoDb is connected"))
     .catch(err => console.log(err))
 
